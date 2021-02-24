0004b764babd055b7996edd6bce5e4e273dcb891
Baraka-AHMED <72963618+Baraka-AHMED@users.noreply.github.com>
2021-02-24 21:17:27 +0100

    24/02

--- Html/js/chat.js
+++ Html/js/chat.js
@@ -1,18 +1,19 @@
 
 
-function openclose(id)
+/* function openclose(id)
 {
-  if (document.getElementById(id).style.display == 'block') {
-       document.getElementById(id).style.display = 'none';
-  }
+ if (document.getElementById(id).style.display == 'block') {
+    document.getElementById(id).style.display = 'none';
+}
   else {
        document.getElementById(id).style.display = 'block';
   }
-}
+}*/
+
 
 const messagepost = document.querySelector('.messagepost')
 const button = document.querySelector('button[type="button"]')
-const input = document.querySelector('input[type="text"]')
+const textarea = document.querySelector('textarea')
 
 function message(msg) {
   let newP = document.createElement('p')
@@ -20,21 +21,37 @@ function message(msg) {
   newP.appendChild(newContent)
   messagepost.append(newP)
 };
-
+function dm (info) {
+  let newspan = document.createElement('span')
+  let newcontent = document.createTextNode(info)
+  newspan.append(newcontent)
+  messagepost.append(newspan)
+}
 function post() {
-    message(input.value)
-    input.value = ''
+  if (textarea.value != 0 || (textarea.value).trim().length) {
+    message(textarea.value)
+    textarea.value = ''
+    date()}
+    
+    
     
     function scrollToBottom() {
-      messageFeed.scrollTop = messageFeed.scrollHeight
+      messagepost.scrollTop = messagepost.scrollHeight
     }
+    
     scrollToBottom()
   };
-  function onRowAdded() {
-    $('.chat-container').animate({
-      scrollTop: $('.chat-container').prop('scrollHeight')
-    });
-  };  
+
+function date (){
+  var date = new Date()
+  var h=date.getHours();
+if (h<10) {h = "0" + h}
+var m=date.getMinutes();
+if (m<10) {m = "0" + m}
+
+  dm("_____"+h+":"+m+"_____")
+
+}
 
 function keyPress(e) {
   if (e.keyCode === 13) {
@@ -45,10 +62,18 @@ function keyPress(e) {
 
     button.className += ' active'
 	removeActive
-    setTimeout(removeActive, 1500000)
+    setTimeout(removeActive, 1000)
     post()
   }
 }
 
 window.addEventListener('keyup', keyPress);
-button.onclick = post;
+document.getElementById("send").onclick = post;
+
+
+function openclose() {
+  $( "#chat" ).slideToggle( "slow" );
+  $(this).find("i").toggleClass("far fa-times");
+};
+
+document.getElementById('msgbutton').onclick = openclose
